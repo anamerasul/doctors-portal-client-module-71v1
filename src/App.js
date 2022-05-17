bc025631bdeec0b2bf54d2c2fed4e5ab11f227a4
anamerasul <anamerasul@gmail.com>
2022-05-17 10:32:12 +0600

    Introduction to Daisy UI to use default NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from "./logo.svg";
+import { Route, Routes } from "react-router";
 import "./App.css";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="">
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+      </Routes>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button class="btn btn-primary">Button</button> */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,4 +4,28 @@ module.exports = {
     extend: {},
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "mytheme",
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "light",
+      "dark",
+      "cupcake",
+    ],
+  },
 };
