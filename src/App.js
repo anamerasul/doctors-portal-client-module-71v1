bcf6c5cc49846fa68f6cc389d21fd151b6888109
anamerasul <anamerasul@gmail.com>
2022-05-17 11:28:17 +0600

    Create simple Top Banner Using Hero component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
 import Header from "./Pages/Shared/Header/Header";
 
 function App() {
@@ -12,9 +13,8 @@ function App() {
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
-      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button class="btn btn-primary">Button</button> */}
     </div>
   );
 }
