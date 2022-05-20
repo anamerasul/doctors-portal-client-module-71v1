ae70d5d633cb415d95cd4c4dff4efb48709f7edc
anamerasul <anamerasul@gmail.com>
2022-05-21 00:17:03 +0600

    Booking Options and available slots with conditional rendering

--- src/Pages/Appoinment/AvailableAppointment.js
+++ src/Pages/Appoinment/AvailableAppointment.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { format } from "date-fns";
+import Service from "./../Appoinment/Service";
 
 const AvailableAppointment = ({ date }) => {
   const [services, setServices] = useState([]);
@@ -10,10 +11,15 @@ const AvailableAppointment = ({ date }) => {
   }, []);
   return (
     <div>
-      {console.log(services)}
       <h4 className="text-xl text-center text-secondary">
         Available Appointment on {format(date, "PP")}
       </h4>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {services.map((service) => (
+          <Service key={service._id} service={service}></Service>
+        ))}
+      </div>
     </div>
   );
 };
